17f38841c0db1e89827e9f73031c7aeb41f7cdf8
nerush.jobs <nerush.kirill@mail.ru>
2023-02-10 16:32:33 +0300

    parsing key from searching

--- program/external_db.py
+++ program/external_db.py
@@ -3,12 +3,15 @@ from config import host_db, user_db, password_db, database_name
 from searching import program_main
 
 
-request_1 = """
+train = program_main(0)
+count_record_day = len(train[0][1])
+
+request_1 = f"""
 INSERT INTO note (iso_number, count_record_day, date_mse, time_start_mse_day, time_end_mse_day, current_date_note, gave)
 VALUES ('661/ВН', 2, '2022-11-07', '09:35', '11:30', '2023-02-10 12.47','И.О. Фамилия')
 """
 
-request_2 = """
+request_2 = f"""
 INSERT INTO mse (id_es_buro, sub_note_id, s_name, f_name, m_name, birthday, time_start_mse, time_end_mse)
 VALUES (5, 3, 'Фамилия', 'Имя', 'Отчество', '1990-01-01', '10:00', '12:00');
 """
--- program/searching.py
+++ program/searching.py
@@ -1,3 +1,4 @@
+from re import split as re_split
 from datetime import datetime
 
 from receiving import receving
@@ -14,7 +15,34 @@ def program_main(n: int):
     current_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
     print(current_date)
+    return [result_pars, current_date]
 
 
 if __name__ == '__main__':
-    program_main(0)
+    train = program_main(0)
+    train_1 = re_split(',\n{0,1} {0,1}', train[0][0][0])
+
+    iso_number = '661/ВН'
+    es_or_buro = train_1[0].split(' ')[0]
+    number_division = train_1[0].split(' ')[1]
+    count_record_day = len(train[0][1])
+    # date_mse =
+    # time_start_mse_day =
+    # time_end_mse_day =
+    # current_date_note =
+    # gave =
+
+    # print('------------------')
+    # print(train_1[1])
+    # print(es_or_buro)
+    # print(number_division)
+    # print(count_record_day)
+
+    # print(iso_number,
+    #       count_record_day,
+    #       date_mse,
+    #       time_start_mse_day,
+    #       time_end_mse_day,
+    #       current_date_note,
+    #       gave,
+    #       )
